062a009556caa740bfe99d77514f5ff7aec9e2c7
MKS-Student <mks.student@hackreactor.com>
2015-10-26 14:43:11 -0700

    [feature] built out main architecture

---
+++ server/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+// var mongoose = require('mongoose');
+// var cors = require('cors');
+var path = require('path');
+// var bodyParser = require('body-parser');
+
+var app = express();
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.listen(3000);
+
+module.exports = app;
